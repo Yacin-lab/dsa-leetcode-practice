687a99239579680cc68ed1b23c1c0930da2625fb
Yacin-lab <baadiyassine51@gmail.com>
2026-04-05 16:23:11 +0100

    "done"

--- sliding-window/maximum_nbr_of_vowles.c
+++ sliding-window/maximum_nbr_of_vowles.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
     Function: maximumNumberOfVowels
     -------------------------------
@@ -9,42 +10,60 @@
     Use Sliding Window technique.
 
     1. Compute the maximum nbr of vowels in the first window
-    2. 
+    2. Check leaving and entering element:
+        if entering == vowels increment +1
+        if leaving == vowels decrement -1
+    3. Update maxCount
 */
-int maximumNumberOfVowels(char* s, int k) {
-    int count = 0;
-    int index;
 
+int checkVowels(char c) {
+    return (c == 'a' || c == 'u' || 
+            c == 'o' || c == 'e' || 
+            c == 'i');
+}
+
+int maximumNumberOfVowels(char* s, int k) {
+    int left;
+    int right;
+    int countVowels = 0;
+
+    // first window
     for(int i = 0; i < k; i++) {
-        if(s[i] == 'a' || s[i] == 'u' 
-        || s[i] == 'o' || s[i] == 'e' 
-        || s[i] == 'i')
-            count++;
+        if(checkVowels(s[i]))
+            countVowels++;
     }
 
-    int maxCount = count;
-    count = 0;
-    int left = 0;
-    int right = k;
-    while(s[right] != '\0') { 
-        count = 0;
-        for(int i = left; i < right; i++) {
-            if(s[i] == 'a' || s[i] == 'u' 
-            || s[i] == 'o' || s[i] == 'e' 
-            || s[i] == 'i')
-                count++;
-            // track maixumum
-        }
-        left++;
+    int maxCount = countVowels;
+    
+    // slide window
+    right = k;
+    while(s[right] != '\0') {
+
+        // jouj elements li kayhmoni --> li 3ad khrej w 3ad dkhl f window
+        // ila kan li khrej vowles n9as mn CountVowels -1
+        left = right - k;
+        if (checkVowels(s[left]))
+            countVowels--;
+        
+
+        // ila kan li dkhel vowles zid l CountVowels +1
+        if (checkVowels(s[right]))
+            countVowels++;
+
+        // update maxCount
+        if (countVowels > maxCount)
+            maxCount = countVowels;
+
         right++;
     }
+    return (maxCount);
 }
 
 int main(void) {
     char arr[] = "abciiidef";
 	int k = 3;
 
-	maximumNumberOfVowels(arr, k);
+	printf("%d\n", maximumNumberOfVowels(arr, k));
 
     return (0);
 }
